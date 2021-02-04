4cfedd8623758dda51cefefd9b97317503cc81f5
Darren Richer <darren.richer@gmail.com>
2021-02-04 09:29:30 -0500

    Installer fix

--- install/install_mosquitto.sh
+++ install/install_mosquitto.sh
@@ -1,24 +1,16 @@
-wget http://repo.mosquitto.org/debian/mosquitto-repo.gpg.key
-sudo apt-key add mosquitto-repo.gpg.key
-cd /etc/apt/sources.list.d/
-sudo wget http://repo.mosquitto.org/debian/mosquitto-buster.list
+#!/bin/bash
+set -e
+
+echo "Installing Mosquitto (this only needs to be done on the master)"
+
 sudo apt-get update
-sudo apt-get install mosquitto mosquitto-clients mosquitto-dev libmosquitto-dev
+sudo apt-get --yes install mosquitto mosquitto-dev 
 
-#
-# Launcher
-#
-#  mosquitto -c mosquitto.conf -d
-
-#
-# After install test:
-#
-
+# Test examples
 # mosquitto_sub -t '#' -u swimcam -P swimming -v
 # mosquitto_pub -t swimcam/start -m 'RESET' -u swimcam -P swimming
 
-# ensure pip is up to data and install paho Python paho library
-#
-sudo python3 -m pip install --upgrade pip
-sudo pip3 install paho-mqtt
+echo "Mosquitto installed. See SwimCam docs on how to enable security."
+
+
 
--- install/swimcam.sh
+++ install/swimcam.sh
@@ -1,2 +1,26 @@
-sudo apt-get install meson ninja-build build-essential autotools-dev libglib2.0-dev pkg-config python3 git vim gstreamer1.0-tools gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav libglib2.0-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
+#!/bin/bash
+set -e
+
+# verify internet connection
+curl https://www.github.com
+
+if [ $? -ne 0 ]; then
+echo "Unable to connect to the internet.  Please check your connection and try again."
+exit -1
+fi
+
+sudo apt-get update
+
+# install dependencies
+
+sudo apt-get --yes install meson ninja-build build-essential autotools-dev libglib2.0-dev pkg-config python3 git gstreamer1.0-tools gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav libglib2.0-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstrtspserver-1.0-dev gir1.2-gst-rtsp-server-1.0 libgstrtspserver-1.0-0 libmosquitto-dev mosquitto-clients
+
+# Ensure pip3 is installed/up to date and install the paho mqtt client
+
+sudo python3 -m pip install --upgrade pip
+sudo pip3 install paho-mqtt
+
+# success
+cd ../
+echo "Swimcam dependencies were successfully installed..."
 
